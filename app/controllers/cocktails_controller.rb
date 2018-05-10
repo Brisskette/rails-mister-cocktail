addb0b12d9ec21429dc6e41b237fb82d6963530f
Antoine Brissy <antoinebrissy@gmail.com>
2018-05-10 18:25:37 +0200

    cocktail controller ok

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,27 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+ def new
+  @cocktail = Cocktail.new
+ end
+
+ def create
+  # raise
+  @cocktail = Cocktail.new(user_params)
+  @cocktail.save
+  redirect_to cocktail_path(@cocktail)
+ end
+
+ private
+
+  def user_params
+    params.require(:cocktail).permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # root to: 'cocktails#index', as: :index
+
+  get '/cocktails', to: "cocktails#index", as: :cocktails
+
+  get 'cocktails/new', to: "cocktails#new"
+
+  post 'cocktails', to: "cocktails#create"
+
+  get 'cocktails/:id', to: "cocktails#show", as: :cocktail
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
